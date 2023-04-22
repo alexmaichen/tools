92dd4bd7243afadd1a4712168d7b16f6873a4536
alexmaichen <108679492+alexmaichen@users.noreply.github.com>
2023-04-22 17:53:09 +0200

    Update tools.py

--- tools.py
+++ tools.py
@@ -4,21 +4,19 @@ Author: Alexmaichen
 Encoding: ASCII
 """
 
+
 #seqSearch() is untested, should work though. in fact it likely even works for searching a sequence within a list!
 def seqSearch(text: str, string: str) -> list: #returns list of all positions from which the sequence can be read
-    #could have done some .split(" ") shenanigans here but thought I'd rather consider the more general case instead as opposed to strictly a search by word
     
     if (type(text)!= str and type(text) != list) or type(text) != tuple:
         raise TypeError
     
     positions = []
     
-    if len(string) > len(text):
-        print("sequence is longer than text, no instances found")
+    if len(string) > len(text):     #sequence is longer than text, no instances found
         return positions
     
-    if not len(string) * len(text):
-        print("sequence and / or text have length 0")
+    if not len(string) * len(text): #sequence and / or text have length 0
         return positions
     
     for i in range(len(text) - len(str) + 1):
@@ -31,6 +29,7 @@ def seqSearch(text: str, string: str) -> list: #returns list of all positions fr
             positions.append(i)
         
     return positions
+
 
 #somewhat inefficient O(n) but easy to remember and use, can even reverse order without the need for the .reverse() method!
 def selection(L: list, order = 'a') -> list:
@@ -50,10 +49,15 @@ def selection(L: list, order = 'a') -> list:
                     L[i], L[j] = L[j], L[i]
     return L
 
+
 #greedy algorithm
 def greedy(somme: float, pieces: list) -> float: #Values in list must be of types int or float. Must not include 0.
+    
     if (type(somme) != float and type(somme) != int) or (type(pieces) != list and type(pieces) != tuple):
         raise TypeError
+    if 0 in pieces:
+        raise ValueError("list contains value 0")
+    
     pieces = selection(pieces, 'd')
     total  = []
     for piece in pieces:
@@ -64,16 +68,19 @@ def greedy(somme: float, pieces: list) -> float: #Values in list must be of type
     return total
 
 
+
 def doubles(L: list) -> bool: #checks if an entry from a sequence appears at least twice in a row
     
     if type(L) != list and type(L) != tuple and type(L) != str:
         raise TypeError
     
     isDouble = False
-    for i in range(len(L) - 1):
-        if L[i] == L[i + 1]:
-            isDouble = True
+    if not len(L) < 2:
+        for i in range(len(L) - 1):
+            if L[i] == L[i + 1]:
+                isDouble = True
     return isDouble
+
 
 #didn't want to copypaste ord(keyword[0]) >= ord('0') and ord(keyword[0]) <= ord('9') everywhere in another piece of code so I wrote this instead
 def isStrIntPart(keyword: str) -> bool: #takes the first character of a string
@@ -83,6 +90,7 @@ def isStrIntPart(keyword: str) -> bool: #takes the first character of a string
         
     check = ord(keyword[0]) >= ord('0') and ord(keyword[0]) <= ord('9')
     return check #returns true if keyword[0] is an int as a str, else false
+
 
 #more complete / accurate version of isStrIntPart(), because isStrIntPart() makes a dangerous assumption
 def isStrInt(keyword: str) -> bool: #less optimized for some scenarios, but may sometimes be required so I wrote it anyways
@@ -94,6 +102,7 @@ def isStrInt(keyword: str) -> bool: #less optimized for some scenarios, but may 
     for i in len(keyword):
         check *= ord(keyword[i]) >= ord('0') and ord(keyword[i]) <= ord('9')
     return check
+
 
 #ever wanted to make a videogame for which you store the chambers in a list, and wish to randomize their order of appearance? well now you can.
 def shuffle(string: str) -> str: #lists are supported too! :]
@@ -118,7 +127,8 @@ def shuffle(string: str) -> str: #lists are supported too! :]
     return s
 
 
-def charRemoveAll(string: str, charToRemove: str) -> str: #remove all instances of a given char from a given string
+#remove all instances of a given char from a given string
+def charRemove(string: str, charToRemove: str) -> str:
     
     if type(string) != str or type(charToRemove) != str:
         raise TypeError
@@ -131,14 +141,34 @@ def charRemoveAll(string: str, charToRemove: str) -> str: #remove all instances 
             w += c
     return w
 
-#needed an alias for the min() function because I always forget to use it otherwise
-def minVal(value: float, limit: float) -> float:
-    return max((value, limit))
 
-#needed an alias for the max() function because I always forget to use it otherwise
-def maxVal(value: float, limit: float) -> float:
-    return min((value, limit))
+#example usecase: x = upperBound(x, 10)
+def upperBound(value: float, limit: float) -> float:
+    if(value < limit):
+        return value
+    return limit
+
+
+#example usecase: x = lowerBound(0, x)
+def lowerBound(limit: float, value: float) -> float:
+    if(value > limit):
+        return value
+    return limit
+
+
+#like the two functions before this, but in one. makes for more readable code
+def bounds(lowerLim: float, value: float, higherLim: float) -> float:
+
+    if lowerLim > upperLim:
+        raise ValueError("Lower-bound is greater than upper-bound.")
+    
+    if (lowerLim > value):
+        return lowerLim
+    if (higherLim < value):
+        return higherLim
+    return value
+
 
 #the math library has this but better. I know.
 def intAndFracParts(n: float) -> tuple:
-    return n//1, n - n//1
+    return int(n//1), n%1
